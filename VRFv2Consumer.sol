088e202b83218291bccb7f1435af33e5ad8d22f1
Samuel Uy <74288590+samueluy@users.noreply.github.com>
2023-07-06 11:02:21 +0800

    removed comment. hardcode values changed initial

--- VRFv2Consumer.sol
+++ VRFv2Consumer.sol
@@ -12,12 +12,6 @@ import "./NFTarf.sol";
  * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
  */
 
-/**
- * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
- * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
- * DO NOT USE THIS CODE IN PRODUCTION.
- */
-
 contract VRFv2Consumer is VRFConsumerBaseV2, ConfirmedOwner {
     event RequestSent(uint256 requestId, uint32 numWords);
     event RequestFulfilled(uint256 requestId, uint256[] randomWords);
